95988ee7cdf2004d009f34189377d53eb9a985b8
Mithlesh kumar <146717835+mithleshkumar96@users.noreply.github.com>
2024-09-23 18:57:10 +0000

    date

--- 01_basics/07_date.js
+++ 01_basics/07_date.js
@@ -1,3 +1,34 @@
-let myDate = new myDate()
-console.log(myDate.toString());
-console.log(myDate.toDateString());
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreateDate=new Date(2023,0,23);
+// console.log(myCreateDate.toDateString());
+// let myCreateDate =new Date(2023,0,23,5,3);
+// console.log(myCreateDate.toDateString());
+// console.log(myCreateDate.toLocaleString());
+// console.log(myCreateDate.toLocaleDateString());
+// console.log(myCreateDate.toISOString());
+// console.log(myCreateDate.toLocaleTimeString());
+
+// let myCreateDate = new Date("09-24-2024");
+// // console.log(myCreateDate.toLocaleString());
+
+// let myTimeStamp =Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate =new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
+
+
+
+
+
+
