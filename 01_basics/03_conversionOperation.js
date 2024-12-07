946b1a703316f61bb6b99fd96f8e4b3411e61144
mithleshkumar95 <mithleshkumarbxr95@gmail.com>
2024-12-07 18:10:57 +0530

    Object

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,11 @@
-let score ="33"
-console.log(typeof score);
-console.log(typeof (score));
+// let score ="33"
+// console.log(typeof score);
+// console.log(typeof (score));
 
+// let valueInNumber=Number(score)
+// console.log(valueInNumber);
 
-let valueInNumber=Number(score)
-console.log(valueInNumber);
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof someNumber);
--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,18 +1,46 @@
-console.log(2>1);
-console.log(2<1);
-console.log(2>=1);
-console.log(2<=1);
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
 
+// console.log("2"<1);
+// console.log("2">1);
+// console.log(1<"2");
 
-console.log("2"<1);
-console.log("2">1);
-console.log(1<"2");
+// console.log(null >0);
+// console.log(null ==0);
+// console.log(null >=0);
 
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
 
-console.log(null >0);
-console.log(null ==0);
-console.log(null >=0);
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
 
-console.log(undefined==0);
-console.log(undefined>0);
-console.log(undefined<0);
+const myName = "text";
+var obj1 = {
+  name: "sas",
+  age: 12,
+  myName: "bala",
+};
+
+var obj2 = {
+  name: "new name",
+  age: 12,
+  [myName]: "bala2",
+};
+const finalObe = { ...obj1, ...obj2 };
+
+console.log(finalObe); //false
+
+const arr1 = []; //32313
+const arr2 = []; //23232
+  console.log(arr1 == arr2); //false  //
+  console.log(arr1 === arr2); //fasle
+  console.log(obj1 === obj2); //false
+const a = 10;
+var b = "10";
+  console.log(a == b); // true
+  console.log(a === b); // false
