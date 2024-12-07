82dfaf8ad8aedc14110bae263789162949201597
mithleshkumar95 <mithleshkumarbxr95@gmail.com>
2024-12-07 20:37:38 +0530

    Operator

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -19,28 +19,61 @@
 // let negValue = -value;
 // console.log(negValue);
 
-const myName = "text";
-var obj1 = {
-  name: "sas",
-  age: 12,
-  myName: "bala",
-};
+// const myName = "text";
+// var obj1 = {
+//   name: "sas",
+//   age: 12,
+//   myName: "bala",
+// };
 
-var obj2 = {
-  name: "new name",
-  age: 12,
-  [myName]: "bala2",
-};
-const finalObe = { ...obj1, ...obj2 };
+// var obj2 = {
+//   name: "new name",
+//   age: 12,
+//   [myName]: "bala2",
+// };
+// const finalObe = { ...obj1, ...obj2 };
 
-console.log(finalObe); //false
+// console.log(finalObe); //false
 
-const arr1 = []; //32313
-const arr2 = []; //23232
-  console.log(arr1 == arr2); //false  //
-  console.log(arr1 === arr2); //fasle
-  console.log(obj1 === obj2); //false
-const a = 10;
-var b = "10";
-  console.log(a == b); // true
-  console.log(a === b); // false
+// const arr1 = []; //32313
+// const arr2 = []; //23232
+// console.log(arr1 == arr2); //false  //
+// console.log(arr1 === arr2); //fasle
+// console.log(obj1 === obj2); //false
+// const a = 10;
+// var b = "10";
+// console.log(a == b); // true
+// console.log(a === b); // false
+
+// let str1 = 5;
+// let str2 = 10;
+// let str3 = str1 + str2;
+// console.log(str3);
+// let str4 = "Mithlesh";
+// let str5 = "  Hello";
+// let str6 = str1 + str2;
+// console.log(str6);
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 ** 3);
+// console.log(2 % 5);
+// console.log(2 / 5);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + "2");
+// console.log(1 + "2");
+// console.log("3" + "2" + 2);
+// console.log(true);
+// console.log(false);
+// console.log(-true);
+// console.log(+true);
+
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 === 1);
+console.log(2 != 1);
